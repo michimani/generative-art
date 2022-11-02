b4d333f79c71b7bd0e2e4fc0fb3db3a4e274a971
michimani <michimani210@gmail.com>
2022-11-03 00:32:17 +0900

    create main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let method = &args[1];
+    render(&method);
+}
+
+fn render(method: &str) {
+    match method {
+        _ => println!("method {} is not found.", method),
+    }
 }
